0443c1b66a35d7448484e74cf264370372dc4654
YoussefTurkey <youssef.turkey11@gmail.com>
2025-08-19 17:49:39 +0300

    edit width of faqs

--- src/app/components/sections/FAQs.tsx
+++ src/app/components/sections/FAQs.tsx
@@ -106,7 +106,7 @@ const FAQs = () => {
         initial={{ opacity: 0, y: 50 }}
         animate={inView ? { opacity: 1, y: 0 } : {}}
         transition={{ duration: 0.8, ease: "easeOut" }}
-        className="w-full md:w-200"
+        className="w-full lg:w-200"
       >
         {faq.map((quest) => (
           <div
