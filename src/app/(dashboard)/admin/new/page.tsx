cbe1194eba2bc87d9a19c129973b91b6dd8b7f98
YoussefTurkey <youssef.turkey11@gmail.com>
2025-11-04 01:30:45 +0200

    dashboard with firebase and cloudnary 5

--- src/app/(dashboard)/admin/new/page.tsx
+++ src/app/(dashboard)/admin/new/page.tsx
@@ -31,6 +31,7 @@ export default function AddProjectPage() {
       industryEn: "",
       industryAr: "",
     },
+    link: '',
     processEn: [{ head: "", body: "" }],
     processAr: [{ head: "", body: "" }],
     image: "",
